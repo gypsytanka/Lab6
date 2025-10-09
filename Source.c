449cb3475bd0e294876ccd10b1f49a179ef285ac
Татьяна <tanya.maslova.0707@mail.ru>
2025-10-09 13:07:18 +0300

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_CTYPE, "RUS");
+
+    int num;
+
+    printf("Введите трехзначное число: ");
+    scanf("%d", &num);
+
+    int units = num % 10;         
+    int tens = (num / 10) % 10;   
+
+    printf("Число десятков: %d\n", tens);
+    printf("Число единиц: %d\n", units);
+
+    return 0;
+}
